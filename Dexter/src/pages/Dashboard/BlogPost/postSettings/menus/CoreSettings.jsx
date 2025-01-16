51eca4adad0b35b011194b18bfca954687169a95
Aishat Akinyemi <akinyemiomolola99@gmail.com>
2025-01-16 15:29:25 +0100

    quick fix

--- Dexter/src/pages/Dashboard/BlogPost/postSettings/menus/CoreSettings.jsx
+++ Dexter/src/pages/Dashboard/BlogPost/postSettings/menus/CoreSettings.jsx
@@ -10,7 +10,7 @@ const CoreSettings = () => {
     <div className="lg:mt-2 md:mt-2 max-md:mt-10 md:mb-8 max-md:mb-8 bg-white border border-gray-300 h-fit shadow-md rounded-lg p-4 lg:w-[84%] md:w-[84%] max-md:w-full ">
       <form>
         
-        <div className="flex max-md:flex-col md:flex-col lg:flex-row gap-8">
+        <div className="flex gap-8 max-md:flex-col md:flex-col lg:flex-row">
           {/* First part */}
           <div className="lg:w-[40%] space-y-6">
             <div className="flex flex-col gap-3">
@@ -20,7 +20,7 @@ const CoreSettings = () => {
                 control={control}
                 defaultValue="English US" // Initial value
                 render={({ field }) => (
-                  <select {...field} id="language" className="w-full outline-none focus:border-2 focus:border-gray-500 border border-gray-300 text-gray-400 rounded-lg p-2">
+                  <select {...field} id="language" className="w-full p-2 text-gray-400 border border-gray-300 rounded-lg outline-none focus:border-2 focus:border-gray-500">
                     <option value="" disabled>
                       Select an option
                     </option>
@@ -39,7 +39,7 @@ const CoreSettings = () => {
               control={control}
               defaultValue="Friendly" // Initial value
               render={({ field }) => (
-                <select {...field} id="tone" className="w-full text-gray-400 outline-none focus:border-2 focus:border-gray-500 border border-gray-300 rounded-lg p-2">
+                <select {...field} id="tone" className="w-full p-2 text-gray-400 border border-gray-300 rounded-lg outline-none focus:border-2 focus:border-gray-500">
                   <option value="" disabled>
                     Select an option
                   </option>
@@ -60,7 +60,7 @@ const CoreSettings = () => {
               control={control}
               defaultValue="None"
               render={({ field }) => (
-                <select {...field} id="pointOfView" className="w-full outline-none focus:border-2 focus:border-gray-500 border border-gray-300 text-gray-400 rounded-lg p-2">
+                <select {...field} id="pointOfView" className="w-full p-2 text-gray-400 border border-gray-300 rounded-lg outline-none focus:border-2 focus:border-gray-500">
                   <option value="" disabled>
                     Select an option
                   </option>
@@ -81,7 +81,7 @@ const CoreSettings = () => {
               control={control}
               defaultValue="None"
               render={({ field }) => (
-                <select {...field} id="country" className="w-full text-gray-400 outline-none focus:border-2 focus:border-gray-500 border border-gray-300 rounded-lg p-2">
+                <select {...field} id="country" className="w-full p-2 text-gray-400 border border-gray-300 rounded-lg outline-none focus:border-2 focus:border-gray-500">
                   <option value="" disabled>
                     Select an option
                   </option>
@@ -95,7 +95,7 @@ const CoreSettings = () => {
           </div>
           {/* Second part */}
           <div className="lg:w-[60%] md:w-full space-y-6">
-            <div className="flex gap-3 items-center">
+            <div className="flex items-center gap-3">
               <div className="flex flex-col gap-3 lg:w-[60%] md:w-1/2">
                 <label htmlFor="articleSize" className="text-#545a67] flex items-center text-[12px] gap-2"> Article size</label>
                 <Controller
@@ -103,7 +103,7 @@ const CoreSettings = () => {
                   control={control}
                   defaultValue="Medium"
                   render={({ field }) => (
-                    <select {...field} id="articleSize" className="text-gray-400  outline-none focus:border-2 focus:border-gray-500 border border-gray-300 rounded-lg p-2">
+                    <select {...field} id="articleSize" className="p-2 text-gray-400 border border-gray-300 rounded-lg outline-none focus:border-2 focus:border-gray-500">
                       <option value="" disabled>
                         Select an option
                       </option>
@@ -131,7 +131,7 @@ const CoreSettings = () => {
                   control={control}
                   defaultValue="Anthropic Claude 3 Haiku Popular"
                   render={({ field }) => (
-                    <select {...field} id="aiModel" className="w-full  outline-none focus:border-2  text-gray-400 focus:border-gray-500 border border-gray-300 rounded-lg p-2">
+                    <select {...field} id="aiModel" className="w-full p-2 text-gray-400 border border-gray-300 rounded-lg outline-none focus:border-2 focus:border-gray-500">
                       <option value="" disabled>
                         Select an option  
                       </option>
@@ -150,8 +150,8 @@ const CoreSettings = () => {
            
             
             <div className="flex">
-              <div className=" lg:w-1/2 md:w-full max-md:w-full flex flex-col gap-5">
-                <div className=" flex items-center gap-2">
+              <div className="flex flex-col gap-5  lg:w-1/2 md:w-full max-md:w-full">
+                <div className="flex items-center gap-2 ">
                   <p className="text-#545a67] text-[12px]">
                     Via your API keys
                   </p>
@@ -171,7 +171,7 @@ const CoreSettings = () => {
                   </div>
                 </div>
               </div>
-              <div  className=" lg:w-1/2 md:w-full max-md:w-full  flex flex-col gap-5">
+              <div  className="flex flex-col gap-5  lg:w-1/2 md:w-full max-md:w-full">
                 <div className="flex items-center gap-2">
                   <p>
                     Total cost
@@ -192,7 +192,7 @@ const CoreSettings = () => {
                   control={control}
                   defaultValue="8th & 9th grade eaily understood Popular"
                   render={({ field }) => (
-                    <select {...field} id="humanize" className="w-full text-gray-400 outline-none focus:border-2 focus:border-gray-500 border border-gray-300 rounded-lg p-2">
+                    <select {...field} id="humanize" className="w-full p-2 text-gray-400 border border-gray-300 rounded-lg outline-none focus:border-2 focus:border-gray-500">
                       <option value="" disabled>
                         Select an option
                       </option>
